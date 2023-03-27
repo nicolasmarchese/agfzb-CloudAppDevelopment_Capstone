8fe6e5555f9d92dc8dc90a30d6235c6b21997311
nicolasmarchese <nicomarchese97@gmail.com>
2023-03-27 13:14:34 -0400

    changes made from theia lab environment

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,10 +1,39 @@
 import requests
 import json
-# import related models here
+from .models import CarDealer, DealerReview
 from requests.auth import HTTPBasicAuth
 
 
 # Create a `get_request` to make HTTP GET requests
+
+def get_request(url, api_key=None, **kwargs):
+    #print(kwargs)
+    #print("GET from {} ".format(url))
+    try:
+        
+       params = kwargs
+
+        if api_key:
+            params = dict()
+            params["text"] = kwargs["text"]
+            params["version"] = kwargs["version"]
+            params["features"] = kwargs["features"]
+            params["return_analyzed_text"] = kwargs["return_analyzed_text"]
+            params["language"] = kwargs["language"]
+            # Basic authentication GET
+            response = requests.get(url, params=params, headers={"Content-Type": "application/json"}, 
+                            auth=HTTPBasicAuth("apikey", api_key))
+        else:
+            # Call get method of requests library with URL and parameters
+            # no authentication GET
+            response = requests.get(url, params=params, headers={'Content-Type': 'application/json'})   
+    except:
+        # If any error occurs
+        print("Network exception occurred")
+    status_code = response.status_code
+    #print("With status {} ".format(status_code))
+    json_data = json.loads(response.text)
+    return json_data
 # e.g., response = requests.get(url, params=params, headers={'Content-Type': 'application/json'},
 #                                     auth=HTTPBasicAuth('apikey', api_key))
 
